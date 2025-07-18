6776f0cc0bec36f4c592589071a9b14bd4bb2075
IamSoumyaBiswas <imsoumyabiswas@gmail.com>
2025-07-19 01:09:02 +0530

    check update

---
+++ app/src/main/java/com/example/weatherapp/api/RetrofitInstance.kt
@@ -0,0 +1,13 @@
+package com.example.weatherapp.api
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+    // val API_KEY='0f3fe5ff181e433b993192348251807'
+    private  const val baseUrl = "https://api.weatherapi.com"
+    private fun  getInstance():Retrofit{
+        return Retrofit.Builder().baseUrl(baseUrl).addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
